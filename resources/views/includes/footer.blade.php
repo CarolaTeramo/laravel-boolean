9d0cefa25c4a290b63621e5e2de6d447029a4596
CarolaTeramo <carolateramo@gmail.com>
2019-07-04 18:18:14 +0200

    senza il css

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,94 @@
-<footer>
-  <h1>FOOTER</h1>
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <a class="navbar-logo" href="https://www.boolean.careers/">
+          <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Careers">
+        </a>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">ciao@</a>
+          </li>
+          <li>
+            <a href="#!">02</a>
+          </li>
+          <li>
+            <a href="#!">+39...</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Domande frequenti</a>
+          </li>
+          <li>
+            <a href="#!">Lezione gratuita</a>
+          </li>
+          <li>
+            <a href="#!">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#!">Lavora con noi</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Icons -->
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">facebook</a>
+            <a href="#!">facebook</a>
+            <a href="#!">facebook</a>
+          </li>
+
+        </ul>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+    Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967
+  </div>
+  <!-- Copyright -->
+
 </footer>
+<!-- Footer -->
--- storage/framework/views/f53078ac3dbfa01d26bd1b5dd58d696b224cff33.php
+++ storage/framework/views/f53078ac3dbfa01d26bd1b5dd58d696b224cff33.php
@@ -1,5 +1,33 @@
-<nav>
-  <div class="">
-    <h1>NAVBAR</h1>
-  </div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-logo" href="https://www.boolean.careers/">
+        <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Careers">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">CORSO</a></li>
+        <li><a href="#">METODO</a></li>
+        <li><a href="#">DOPO IL CORSO</a></li>
+        <li><a href="#">LEZIONE GRATUITA</a></li>
+        <li>
+          <form class="navbar-form navbar-right">
+            <button type="submit" class="btn btn-default">Iscriviti</button>
+          </form>
+        </li>
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
